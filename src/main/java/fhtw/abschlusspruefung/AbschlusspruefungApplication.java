9175671ea0356def1e0d7c09803de4060e247a41
derkauzigekoala <derkauzigekoala@gmail.com>
2023-01-26 17:59:34 +0100

    Initial commit

---
+++ src/main/java/fhtw/abschlusspruefung/AbschlusspruefungApplication.java
@@ -0,0 +1,13 @@
+package fhtw.abschlusspruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlusspruefungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlusspruefungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/abschlusspruefung/AbschlusspruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.abschlusspruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlusspruefungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
